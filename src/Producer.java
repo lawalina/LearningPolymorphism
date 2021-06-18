781adf3f5c1751f8f7b2eb8989ab76013bd92b0e
lawalilna <alina.lawrentyeva@gmail.com>
2021-06-18 23:29:12 +0300

    Добавили Vector, Linked List и Array List.

--- src/Producer.java
+++ src/Producer.java
@@ -6,6 +6,10 @@ import Exceptions.EnumMessageException;
 import Exceptions.NoBreedException;
 import Exceptions.NullEqualsException;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
 public class Producer {
     public static void main(String[] args) throws NullEqualsException {
 
@@ -17,12 +21,81 @@ public class Producer {
             String additionalInformationAboutDagon = "This cat isn't afraid of water and loves to make a mess.";
             Animal dagon = new Cat(0.5, 7, "Dagon", additionalInformationAboutDagon);
 
+            //Vector
+
+            Vector animalVector = new Vector();
+
+            animalVector.add(ayka);
+            animalVector.add(sipsik);
+            animalVector.add(strangeDuck);
+            animalVector.add(dagon);
+
+            Vector<GenericAnimal> animalVectorGenerics = new Vector<>();
+
+            for (int i = 0; i < animalVector.size(); i++)
+                animalVectorGenerics.add(new GenericAnimal ((Animal) animalVector.get(i)));
+
+            System.out.println("");
+            System.out.println("This is a Vector!");
+            System.out.println("");
+
+            for (GenericAnimal<Animal> animalVectorGeneric : animalVectorGenerics){
+                animalVectorGeneric.printInfo();
+            }
+
+            //LinkedList
+            LinkedList animalLinkedList = new LinkedList();
+
+            animalLinkedList.add(ayka);
+            animalLinkedList.add(sipsik);
+            animalLinkedList.add(strangeDuck);
+            animalLinkedList.add(dagon);
+
+            LinkedList<GenericAnimal> animalLinkedListGenerics = new LinkedList<>();
+
+            for (int i = 0; i < animalLinkedList.size(); i++)
+                animalLinkedListGenerics.add(new GenericAnimal ((Animal) animalLinkedList.get(i)));
+
+            System.out.println("");
+            System.out.println("This is a Linked List!");
+            System.out.println("");
+
+            for (GenericAnimal<Animal> animalLinkedListGeneric : animalLinkedListGenerics){
+                animalLinkedListGeneric.printInfo();
+            }
+
+            //ArrayList
+            ArrayList animalsArrayList = new ArrayList();
+
+            animalsArrayList.add(ayka);
+            animalsArrayList.add(sipsik);
+            animalsArrayList.add(strangeDuck);
+            animalsArrayList.add(dagon);
+
+            ArrayList<GenericAnimal> animalsArrayListGenerics = new ArrayList<>();
+
+            for (int i = 0; i < animalsArrayList.size(); i++)
+                animalsArrayListGenerics.add(new GenericAnimal ((Animal) animalsArrayList.get(i)));
+
+            System.out.println("");
+            System.out.println("This is a Array List!");
+            System.out.println("");
+
+            for (GenericAnimal<Animal> animalsArrayListGeneric : animalsArrayListGenerics){
+                animalsArrayListGeneric.printInfo();
+            }
+
+            //Simple Array
             Animal[] animals = new Animal[]{ayka, sipsik, strangeDuck, dagon};
 
             GenericAnimal<Animal>[] animalGenerics = new GenericAnimal[animals.length];
 
             for (int i = 0; i < animals.length; i++)
                 animalGenerics[i] = new GenericAnimal<>(animals[i]);
+
+            System.out.println("");
+            System.out.println("This is a simple Array!");
+            System.out.println("");
 
             for (GenericAnimal<Animal> animalGeneric : animalGenerics) {
                 animalGeneric.printInfo();
